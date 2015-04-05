abf56d9d3987ed229dbbefd809c9c091c1acf02f
Mike Dvorscak <mike.dvorscak@gmail.com>
2015-04-05 11:12:47 -0500

    Added minification task to gulpfile

--- gulpfile.js
+++ gulpfile.js
@@ -1,18 +1,30 @@
 /**
  * Created by Mike Dvorscak on 4/4/15.
  */
+'use strict';
 var gulp = require('gulp');
+var uglify = require('gulp-uglify');
+var rename = require('gulp-rename');
 var karma = require('karma').server;
 
-gulp.task('test', function(done){
+gulp.task('test', function (done) {
     karma.start({
         configFile: __dirname + '/karma.conf.js',
-        singleRun: true
+        singleRun : true
     }, done);
 });
 
-gulp.task('tdd', function(done){
+gulp.task('tdd', function (done) {
     karma.start({
         configFile: __dirname + '/karma.conf.js'
     }, done);
+});
+
+gulp.task('scripts', function () {
+    return gulp.src('cloak.js')
+        .pipe(uglify())
+        .pipe(rename({
+                  extname: '.min.js'
+              }))
+        .pipe(gulp.dest(''));
 });
